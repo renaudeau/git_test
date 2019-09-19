617fc1b34686008e1aa0bec62337191c28ae028d
renaudeau <eric.renaudeau15@orange.fr>
2019-09-19 09:30:21 +0200

    revert commit by diff

--- EmptyTestPrj/src/fr/cnamts/renaudeau/test/TestEmpty.java
+++ EmptyTestPrj/src/fr/cnamts/renaudeau/test/TestEmpty.java
@@ -4,7 +4,7 @@ public class TestEmpty {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test 2 git");
+		System.out.println("test git");
 
 	}
 
