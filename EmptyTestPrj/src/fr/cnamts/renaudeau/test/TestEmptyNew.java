6df3e4727324e33bae449286c4b47a50adca225c
renaudeau <eric.renaudeau15@orange.fr>
2019-09-19 09:06:40 +0200

    new commit

---
+++ EmptyTestPrj/src/fr/cnamts/renaudeau/test/TestEmptyNew.java
@@ -0,0 +1,10 @@
+package fr.cnamts.renaudeau.test;
+
+public class TestEmptyNew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("...");
+	}
+
+}
