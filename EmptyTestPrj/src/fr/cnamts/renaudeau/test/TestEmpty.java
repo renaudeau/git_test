671be0ecfd817c9a2790f7dd6729510d5030151e
renaudeau <eric.renaudeau15@orange.fr>
2019-09-19 08:58:48 +0200

    test ini git

---
+++ EmptyTestPrj/src/fr/cnamts/renaudeau/test/TestEmpty.java
@@ -0,0 +1,11 @@
+package fr.cnamts.renaudeau.test;
+
+public class TestEmpty {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test git");
+
+	}
+
+}
